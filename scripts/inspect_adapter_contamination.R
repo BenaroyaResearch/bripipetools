ba66bc202f1a341c652511a3cf7a10b88d977254
James Eddy <james.a.eddy@gmail.com>
2015-12-01 16:35:55 -0800

    updating scripts to inspect latest flowcell

--- scripts/inspect_adapter_contamination.R
+++ scripts/inspect_adapter_contamination.R
@@ -6,19 +6,31 @@ library(ggplot2)
 library(reshape2)
 library(stringr)
 library(ggthemes)
-
+library(Biostrings)
 
 # read adapter count file -------------------------------------------------
 
-adapter_count_file <- "data/adapter_contam.csv"
-adapter_count_dat <- read_csv(adapter_count_file) %>% 
-    .[, -1]
+# adapter_count_file <- "data/adapter_contam.csv"
+# adapter_count_dat <- read_csv(adapter_count_file) %>% 
+#     .[, -1]
 
 # read adapter seq file ---------------------------------------------------
 
-adapter_seq_file <- "data/fastqc_adapter_seqs.csv"
+adapter_seq_file <- "data/fastqc_adapter_seqs_151123.csv"
 adapter_seq_dat <- read_csv(adapter_seq_file) %>% 
     .[, -1]
+
+
+# filter projects of interest ---------------------------------------------
+
+# keep_flowcells <- c("150625_D00565_0089_BHMMCMADXX",
+                    # "150828_D00565_0095_BC6NRPANXX")
+# keep_flowcells <- c("150729_D00565_0092_AC6VC6ANXX")
+keep_flowcells <- c("151123_D00565_0099_BC81MLANXX")
+
+adapter_seq_dat <- adapter_seq_dat %>% 
+    filter(flowcell %in% keep_flowcells) %>% 
+    mutate(lib_id = str_extract(lib_id, "lib[0-9]+"))
 
 # extract adapter match lengths -------------------------------------------
 
@@ -28,12 +40,46 @@ adapter_seq_dat <- adapter_seq_dat %>%
                                                "[0-9]+(?=bp)"))))
 
 
+
+# write file --------------------------------------------------------------
+
+contam_file <- "data/example_fastqc_seqs.csv"
+write_csv(adapter_seq_dat, contam_file)
+# check for PCR primer sequence -------------------------------------------
+
+# primer_seq <- "CAAGCAGAAGACGGCATACGAGAT"
+# # primer_seq <- "AATGATACGGCGACCACCGAGATCTACACTCTTTCCCTACACGA"
+# primer_comp <- complement(DNAString(primer_seq)) %>% as.character()
+# primer_rcomp <- reverseComplement(DNAString(primer_seq)) %>% as.character()
+# 
+# # adapter_seq_dat <- adapter_seq_dat %>% 
+# #     mutate(has_primer = str_detect(primer_seq, seq),
+# #            has_primer_comp = str_detect(primer_comp, seq),
+# #            has_primer_rcomp = str_detect(primer_rcomp, seq))
+# 
+# 
+# test_seq <- adapter_seq_dat %>%
+#     filter(lib_id == "lib7400_HMMCMADXX") %>% 
+#     select(seq) %>% 
+#     as.character()
+# 
+# pairwiseAlignment(DNAString(primer_rcomp), DNAString(test_seq))
+
+# summarize ---------------------------------------------------------------
+
+# filter adapter hits
+adapter_seq_filtered <- adapter_seq_dat %>% 
+    mutate(match = str_replace(match, " \\(.*\\)", "")) %>% 
+    group_by(lib_id, match) %>% 
+    filter(percent == max(percent))
+
 # summarize adapter hits per lib
 
-adapter_seq_summary <- adapter_seq_dat %>% 
+adapter_seq_summary <- adapter_seq_filtered %>% 
     group_by(flowcell, project, lane, prep, lib_id) %>% 
     summarise(num_hits = sum(len > 0),
               mean_len = mean(len),
+              mean_perc = mean(percent),
               total_count = sum(count),
               total_perc = sum(percent))
 
@@ -42,12 +88,12 @@ adapter_seq_summary <- adapter_seq_dat %>%
 # plot adapter seq data ---------------------------------------------------
 
 adapter_seq_summary %>% 
-    filter(flowcell != '150615_D00565_0087_AC6VG0ANXX') %>% 
-    ggplot(aes(x = project, y = num_hits)) +
+    # filter(flowcell != '150615_D00565_0087_AC6VG0ANXX') %>% 
+    ggplot(aes(x = project, y = mean_perc)) +
     geom_point(aes(fill = prep, size = mean_len),
                shape = 21, alpha = 0.5,
                position = position_jitter(width = 0.3, height = 0)) +
-    facet_wrap(~ flowcell, scales = "free_x", ncol = 1) +
+    facet_wrap(~ flowcell, scales = "free_x", ncol = 2) +
     theme(axis.text.x = element_text(angle = -90, hjust = 0)) +
     scale_fill_colorblind()
 
