6b5a73b2725b8ccbbb3d97dabc21ce182309b9bd
James Eddy <james.a.eddy@gmail.com>
2015-12-15 17:14:49 -0800

    making headway on metrics comparison

--- globusgalaxyops/scripts/compare_globus_to_local.R
+++ globusgalaxyops/scripts/compare_globus_to_local.R
@@ -18,6 +18,13 @@ globus_metrics_file <- "data/P43-12_globus_metrics.csv"
 globus_metrics_dat <- read_csv(globus_metrics_file)
 names(globus_metrics_dat)[1] <- "lib_id"
 
+# temp filter
+skip_libs <- c("lib6832", "lib6924")
+local_metrics_dat <- local_metrics_dat %>% 
+    filter(!(lib_id %in% skip_libs))
+
+globus_metrics_dat <- globus_metrics_dat %>% 
+    filter(!(lib_id %in% skip_libs))
 
 # define fxns -------------------------------------------------------------
 
@@ -48,7 +55,7 @@ eucd <- function(x, y) {
     return(sqrt(sum((x - y)^2)))
 }
 
-get_metric_stats <- function(metric_dat) {
+get_metric_stats <- function(metric_dat, norm = FALSE) {
 
     metric_name <- names(metric_dat)[1] %>% 
         str_extract(".*(?=\\.)")
@@ -56,64 +63,130 @@ get_metric_stats <- function(metric_dat) {
     x <- metric_dat[, 1]
     y <- metric_dat[, 2]
     
+    if (norm) {
+        max_val <- max(x, y)
+        x <- x / max_val
+        y <- y / max_val
+    }
+    
 
     metric_df <- data_frame(
         metric = metric_name,
         rho = cor.test(x, y)$estimate,
         eucd = eucd(x, y),
-        rmsd = rmsd(x, y)
+        rmsd = rmsd(x, y),
+        lm_int = lm(y ~ x)$coef[1],
+        lm_slope = lm(y ~ x)$coef[2]
     )
 
     return(metric_df)
 } 
 
-# collect metric stats ----------------------------------------------------
+# all correlations --------------------------------------------------------
 
-metric_stats_dat <- lapply(as.list(2:length(local_metrics_dat)), 
+remove_constant_vars <- function(data) {
+    const_var_names <- sapply(data, n_distinct)
+    const_var_names <- attr(const_var_names[const_var_names == 1], "names")
+    data <- data[, !(names(data) %in% const_var_names)]    
+    return(data)
+}
+
+local_metrics_mat <- local_metrics_dat %>% 
+    select(-contains("HQ")) %>% # the Globus version of Picard does something
+                                # weird when it counts high quality bases;
+                                # ignore these fields for now
+    remove_constant_vars() %>% 
+    .[, -1] %>% 
+    as.matrix()
+
+globus_metrics_mat <- globus_metrics_dat %>% 
+    select(-contains("HQ")) %>% 
+    remove_constant_vars() %>%
+    .[, -1] %>% 
+    as.matrix()
+
+
+# collect metric stats on remaining metrics -------------------------------
+
+sub_local_metrics_dat <- local_metrics_dat %>% 
+    select(-contains("HQ")) %>% # the Globus version of Picard does something
+    # weird when it counts high quality bases;
+    # ignore these fields for now
+    select(-contains("PF")) %>%
+    remove_constant_vars()
+
+sub_globus_metrics_dat <- globus_metrics_dat %>% 
+    select(-contains("HQ")) %>%
+    select(-contains("PF")) %>%
+    remove_constant_vars()
+
+metric_stats_dat <- lapply(as.list(2:length(sub_local_metrics_dat)), 
                            function(x) { 
-                               extract_metric(local_metrics_dat, 
-                                              globus_metrics_dat, x) %>% 
+                               extract_metric(sub_local_metrics_dat, 
+                                              sub_globus_metrics_dat, x) %>% 
                                    get_metric_stats
                            }) %>% 
     bind_rows()
 
 
-# plot problem metrics ----------------------------------------------------
+metric_stats_norm_dat <- lapply(as.list(2:length(sub_local_metrics_dat)), 
+                           function(x) { 
+                               extract_metric(sub_local_metrics_dat, 
+                                              sub_globus_metrics_dat, x) %>% 
+                                   get_metric_stats(norm = TRUE)
+                           }) %>% 
+    bind_rows()
 
-diff_metrics <- metric_stats_dat %>% 
-    filter(log10(rmsd) > 0)
 
-plot_metric_vals <- function(metric_name) {
-    col <- which(names(local_metrics_dat) == metric_name)
-    metric_dat <- extract_metric(local_metrics_dat, globus_metrics_dat, col,
-                                 include_lib = TRUE)
+
+# select offset metrics ---------------------------------------------------
+
+offset_metric_dat <- metric_stats_dat %>% 
+    filter(abs(lm_int) > 0.1)
+
+# simple metric plotting --------------------------------------------------
+
+plot_metric <- function(metric, norm = FALSE, rank = FALSE, rm_outliers = FALSE) {
+    x <- local_metrics_dat[[metric]]
+    y <- globus_metrics_dat[[metric]]
     
-    metric_melt <- metric_dat %>%
-        melt(id.vars = "lib_id", variable.name = "metric_source")
-    
-    if (diff(range(metric_melt$value)) > 1e6) {
-        p <- metric_melt %>% 
-            ggplot(aes(x = lib_id, y = log10(value)))
-    } else {
-        p <- metric_melt %>% 
-            ggplot(aes(x = lib_id, y = value))
+    if (rank) {
+        norm <- FALSE
+        rm_outliers <- FALSE
+        x <- rank(x)
+        y <- rank(y)
     }
     
-    p <- p +
-        geom_point(aes(colour = metric_source), 
-                   position = position_dodge(width = 0.5, height = 0))
+    if (norm) {
+        max_val <- max(x, y)
+        x <- x / max_val
+        y <- y / max_val
+    }
+    
+    if (rm_outliers) {
+        q <- IQR(c(x, y))
+        m <- median(c(x, y))
+        x <- x[ (x >= (m - 0.75*q)) & (x <= (m + 0.75*q)) ]
+        y <- y[ (y >= (m - 0.75*q)) & (y <= (m + 0.75*q)) ]
+        print(m - 0.75*q)
+        print(m + 0.75*q)
+    }
+
+    plot(x, y, main = metric, xlab = "local", ylab = "globus")
+    abline(0, 1)
 }
 
-dev.off()
-lapply(as.list(diff_metrics$metric), plot_metric_vals)
+# hq base check -----------------------------------------------------------
 
-# Bland-Altman plot
+lib <- "lib6833"
 
-# n1 = as.name(names(metric_dat)[1])
-# n2 = as.name(names(metric_dat)[2])
-# dots = list(interp(~x - y, x = n1, y = n2),
-#             interp(~(x + y) / 2, x = n1, y = n2))
-# metric_dat %>% 
-#     mutate_(.dots = setNames(dots, c("diff", "mean"))) %>% 
-#     ggplot(aes(x = mean, y = diff)) +
-#     geom_point()
+gb <- globus_metrics_dat %>% filter(lib_id == lib) %>% .$PF_ALIGNED_BASES
+lb <- local_metrics_dat %>% filter(lib_id == lib) %>% .$PF_ALIGNED_BASES
+lbh <- local_metrics_dat %>% filter(lib_id == lib) %>% .$PF_HQ_ALIGNED_BASES
+gbh <- globus_metrics_dat %>% filter(lib_id == lib) %>% .$PF_HQ_ALIGNED_BASES
+
+lr <- local_metrics_dat %>% filter(lib_id == lib) %>% .$PF_READS_ALIGNED
+gr <- globus_metrics_dat %>% filter(lib_id == lib) %>% .$PF_READS_ALIGNED
+lrh <- local_metrics_dat %>% filter(lib_id == lib) %>% .$PF_HQ_ALIGNED_READS
+grh <- globus_metrics_dat %>% filter(lib_id == lib) %>% .$PF_HQ_ALIGNED_READS
+
