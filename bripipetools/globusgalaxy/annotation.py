b46ca5c683d372deb6543a1c2b50e35f607bf233
James Eddy <james.a.eddy@gmail.com>
2016-05-18 13:10:34 -0700

    cleaning up variable names & docstrings for consistency

--- bripipetools/globusgalaxy/annotation.py
+++ bripipetools/globusgalaxy/annotation.py
@@ -18,44 +18,47 @@ class GlobusOutputAnnotator(object):
         self.output_map = sample_output_map[sample]
         self.sample = sample
 
-    def get_output_type(self, output_name):
+    def get_output_type(self, output_id):
         """
         Return the type of a selected output.
 
-        :type output_name: str
-        :param output_name: Parameter name of the current output file (e.g.,
-            'tophat_alignments_bam_out')
+        :type output_id: str
+        :param output_id: Parameter name of the current output file (e.g.,
+            'tophat_alignments_bam')
 
         :rtype: str
-        :return: A string indicating the filetype (extension) of the output
-            file (e.g., 'bam').
+        :return: A string indicating the result type of the output file (e.g.,
+            'alignments' for 'tophat_alignments_bam').
         """
-        output_name = re.sub('_[a-z]+$', '', output_name)
-        return strings.matchdefault('(?<=_)[a-z]+$', output_name, output_name)
+        # trim trailing characters following last '_'
+        output_id = re.sub('_[a-z]+$', '', output_id)
 
-    def get_output_source(self, output_name):
+        # match characters following last remaining '_' or return whole string
+        return strings.matchdefault('(?<=_)[a-z]+$', output_id, output_id)
+
+    def get_output_source(self, output_id):
         """
         Return the source of or tool used to generate the current output file.
 
-        :type output_name: str
-        :param output_name: Parameter name of the current output file (e.g.,
-            'tophat_alignments_bam_out')
+        :type output_id: str
+        :param output_id: Parameter name of the current output file (e.g.,
+            'tophat_alignments_bam')
 
         :rtype: str
         :return: A string indicating the source of the output file (e.g.,
             'tophat').
         """
         # TODO: replace with separate parameter configs (json or yaml)
-        if not re.search('fastq$', output_name):
+        if not re.search('fastq$', output_id):
             output_sources = ['picard_align', 'picard_markdups', 'picard_rnaseq',
                               'htseq', 'trinity', 'tophat', 'tophat_stats',
                               'fastqc', 'workflow_log']
             return [s for s in output_sources
-                    if re.search(s.lower(), output_name)][0]
+                    if re.search(s.lower(), output_id)][0]
         else:
             output_sources = ['fastq', 'trimmed_fastq']
             return [s for s in output_sources
-                    if re.search('^' + s.lower() + '$', output_name)][0]
+                    if re.search('^' + s.lower() + '$', output_id)][0]
 
     def get_output_info(self):
         """
@@ -64,22 +67,15 @@ class GlobusOutputAnnotator(object):
 
         :rtype: dict
         :return: A dict, where for each ``source`` key, a list of dicts is
-            stored, containing the ``file`` (path) and ``type`` of each
+            stored, containing the ``path`` and ``type`` of each
             corresponding output file.
         """
         source_output_map = {}
-        for o in self.output_map:
-            output_type = self.get_output_type(o)
-            output_source = self.get_output_source(o)
+        for output_id in self.output_map:
+            output_type = self.get_output_type(output_id)
+            output_source = self.get_output_source(output_id)
 
-            source_output_map.setdefault(output_source, {})[o] = \
-                {'file': self.output_map[o], 'type': output_type}
-
-            # if rs in source_dict:
-            #     source_output_map[rs][o] = {'file': output_dict[o],
-            #                           'type': rt}
-            # else:
-            #     source_dict[rs] = {o: {'file': output_dict[o],
-            #                            'type': rt}}
+            source_output_map.setdefault(output_source, {})[output_id] = \
+                {'path': self.output_map[output_id], 'type': output_type}
 
         return source_output_map
