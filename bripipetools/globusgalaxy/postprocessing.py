0e135630d1ff0d9e7b62fea1cae8fe50bdb3d382
James Eddy <james.a.eddy@gmail.com>
2016-05-20 14:18:21 -0700

    added small method to get full path for batch file

--- bripipetools/globusgalaxy/postprocessing.py
+++ bripipetools/globusgalaxy/postprocessing.py
@@ -19,15 +19,31 @@ class GlobusOutputManager(object):
                                              "globus_batch_submission")
 
     def _select_batch_prompt(self):
+        """
+        Display the command-line prompt for selecting an individual batch file.
+
+        :rtype: str
+        :return: String collected from ``raw_input`` in response to the prompt.
+        """
         return ui.prompt_raw("Select batch to process: ")
 
     def _select_batch_date_prompt(self):
+        """
+        Display command-line prompt for selecting batches by date.
+
+        :rtype: str
+        :return: String collected from ``raw_input`` in response to the prompt.
+        """
         return ui.prompt_raw("Select date of batch(es) to process: ")
 
     def _select_batches(self):
         """
-        Select and store path to batch submit file(s) from among list of
+        Select and store names of batch submit file(s) from among list of
         batches found in the 'globus_batch_submission' folder.
+
+        :rtype: list
+        :return: A list with the name of the batch file corresponding to user
+            command-line selection.
         """
         # TODO: add multi-select
         batch_submit_dir = self.batch_submit_dir
@@ -37,13 +53,18 @@ class GlobusOutputManager(object):
         ui.list_options(batch_files)
         batch_i = ui.input_to_int(self._select_batch_prompt)
         batch_file = batch_files[batch_i]
-        return batch_file
-        #
-        # return [os.path.join(batch_submit_dir, f)
-        #         for f in os.listdir(batch_submit_dir)
-        #         if f.split('_')[0] in batch_files]
+        return [batch_file]
 
     def _select_date_batches(self):
+        """
+        Select and store names of batch submit file(s) corresponding to a
+        specified date among batches found in the 'globus_batch_submission'
+        folder.
+
+        :rtype: list
+        :return: A list with the name of the batch file corresponding to user
+            command-line selection.
+        """
         batch_submit_dir = self.batch_submit_dir
         batch_dates = list(set(f.split('_')[0]
                                for f in os.listdir(batch_submit_dir)
@@ -56,3 +77,16 @@ class GlobusOutputManager(object):
 
         return [f for f in os.listdir(batch_submit_dir)
                 if f.split('_')[0] in batch_date]
+
+    def _get_batch_file_path(self, batch_file_name):
+        """
+        Return full path for batch file by joining with ``batch_submit_dir``.
+
+        :type batch_file_name: str
+        :param batch_file_name: File name for a batch submit file.
+
+        :rtype: str
+        :return: Full path to batch file.
+        """
+        batch_submit_dir = self.batch_submit_dir
+        return os.path.join(batch_submit_dir, batch_file_name)
--- tests/test_globusgalaxy.py
+++ tests/test_globusgalaxy.py
@@ -32,11 +32,17 @@ class TestGlobusOutputManager:
     def test_select_batches_0(self):
         with mock.patch('__builtin__.raw_input', return_value="0"):
             assert(globus_output_manager()._select_batches() ==
-                   ("160216_P109-1_P14-12_C6VG0ANXX_"
-                    "optimized_truseq_unstrand_sr_grch38_v0.1_complete.txt"))
+                   [('160216_P109-1_P14-12_C6VG0ANXX_'
+                     'optimized_truseq_unstrand_sr_grch38_v0.1_complete.txt')])
 
     def test_select_date_batches_1(self):
         with mock.patch('__builtin__.raw_input', return_value="1"):
             assert(globus_output_manager()._select_date_batches() ==
-                   [("160411_P43-12_C6VG0ANXX_"
-                    "optimized_nextera_sr_grch38_v0.1_complete.txt")])
+                   [('160411_P43-12_C6VG0ANXX_'
+                     'optimized_nextera_sr_grch38_v0.1_complete.txt')])
+
+    def test_get_batch_file_path_dummy_file(self):
+        assert(globus_output_manager()._get_batch_file_path('dummy.txt') ==
+               ('./tests/test-data/genomics/Illumina/'
+                '150615_D00565_0087_AC6VG0ANXX/globus_batch_submission/'
+                'dummy.txt'))
