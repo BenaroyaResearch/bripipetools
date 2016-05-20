0382afc051282ca23ccb611b4c744168419dc6c0
James Eddy <james.a.eddy@gmail.com>
2016-05-20 11:58:22 -0700

    added prompt functions for selecting batch or batch date

--- bripipetools/globusgalaxy/postprocessing.py
+++ bripipetools/globusgalaxy/postprocessing.py
@@ -4,6 +4,7 @@ Organize, format, and clean up outputs from a Globus Galaxy processing run.
 
 import os
 
+from bripipetools.util import ui
 from bripipetools.globusgalaxy import curation
 
 class GlobusOutputManager(object):
@@ -16,23 +17,32 @@ class GlobusOutputManager(object):
         self.batch_submit_dir = os.path.join(self.flowcell_dir,
                                              "globus_batch_submission")
 
+    def _select_batch_prompt(self):
+        return ui.prompt_raw("Select batch to process: ")
+
+    def _select_batch_date_prompt(self):
+        return ui.prompt_raw("Select date of batch(es) to process: ")
+
     def _select_batches(self):
         """
         Select and store path to batch submit file(s) from among list of
-        batches found in the 'globus_batch_submission' folder. 
+        batches found in the 'globus_batch_submission' folder.
         """
         # TODO: add multi-select
         batch_submit_dir = self.batch_submit_dir
         batch_files = os.listdir(batch_submit_dir)
         print "\nFound the following Globus Genomics Galaxy batches:"
-        for i, d in enumerate(batch_files):
-            print "%3d : %s" % (i, d)
-        batch_i = raw_input("Select a batch to compile: ")
-        batch_file = batch_files[int(batch_i)]
-
-        return [os.path.join(batch_submit_dir, f)
-                for f in os.listdir(batch_submit_dir)
-                if f.split('_')[0] in batch_files]
+        # for i, d in enumerate(batch_files):
+        #     print "%3d : %s" % (i, d)
+        ui.list_options(batch_files)
+        # batch_i = raw_input("Select a batch to compile: ")
+        batch_i = ui.input_to_int(self._select_batch_prompt)
+        batch_file = batch_files[batch_i]
+        return batch_file
+        #
+        # return [os.path.join(batch_submit_dir, f)
+        #         for f in os.listdir(batch_submit_dir)
+        #         if f.split('_')[0] in batch_files]
 
     def _select_date_batches(self):
         batch_submit_dir = self.batch_submit_dir
--- tests/test_globusgalaxy.py
+++ tests/test_globusgalaxy.py
@@ -14,7 +14,21 @@ def globus_output_manager():
 class TestGlobusOutputManager:
     def test_init(self):
         assert(globus_output_manager())
-
-    def test_init_attr(self):
         assert('flowcell_dir' in dir(globus_output_manager()))
         assert('batch_submit_dir' in dir(globus_output_manager()))
+
+    def test_select_batch_prompt(self, capsys):
+        with mock.patch('__builtin__.raw_input', return_value=""):
+            globus_output_manager()._select_batch_prompt()
+            out, err = capsys.readouterr()
+            assert(err == "Select batch to process: \n")
+
+    def test_select_batch_date_prompt(self, capsys):
+        with mock.patch('__builtin__.raw_input', return_value=""):
+            globus_output_manager()._select_batch_date_prompt()
+            out, err = capsys.readouterr()
+            assert(err == "Select date of batch(es) to process: \n")
+
+    def test_select_batches_0(self, capsys):
+        with mock.patch('__builtin__.raw_input', return_value="0"):
+            assert(globus_output_manager()._select_batches() == 0)
