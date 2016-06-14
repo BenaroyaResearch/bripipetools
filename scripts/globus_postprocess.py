0af1a32782f7f6cc9619b606298c78fa10ee3a54
James Eddy <james.a.eddy@gmail.com>
2016-06-14 16:01:11 -0700

    added script to parse args and call globus postprocessing submodule

---
+++ scripts/globus_postprocess.py
@@ -0,0 +1,35 @@
+import _mypath
+import sys, os, argparse, time, json, re
+from bripipetools.globusgalaxy import postprocessing
+
+def main(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--flowcell_dir',
+                        required=True,
+                        default=None,
+                        help=("specify the directory of unaligned library "
+                              "FASTQs to be processed - e.g., "
+                              "/mnt/genomics/Illumina/"
+                              "150615_D00565_0087_AC6VG0ANXX/Unaligned"
+                              "P43-12-23224208"))
+    parser.add_argument('-c', '--check_only',
+                        action='store_true',
+                        help=("check that all outputs are present, but don't "
+                              "attempt to compile"))
+    parser.add_argument('-d', '--dry_run',
+                        action='store_true',
+                        help=("only report what would be done"))
+    args = parser.parse_args()
+
+    flowcell_dir = args.flowcell_dir
+    check_only = args.check_only
+    dry_run = args.dry_run
+
+    out_manager = postprocessing.GlobusOutputManager(flowcell_dir)
+    out_manager.check_batches()
+    if not check_only:
+        out_manager.curate_batches(dry_run=dry_run)
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
