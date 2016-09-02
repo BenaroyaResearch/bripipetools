60c45c84636ce1c19978477e1e67543c8c35a1bd
James Eddy <james.a.eddy@gmail.com>
2016-09-01 17:07:53 -0700

    add methods for collecting lists of projects and libraries for flowcell run; add new annotation class (no methods yet) for sequenced libraries

--- bripipetools/annotation/illuminaseq.py
+++ bripipetools/annotation/illuminaseq.py
@@ -10,6 +10,7 @@ import os
 import re
 import bripipetools.model as docs
 from bripipetools.util import files
+from bripipetools.parsing import illumina
 
 class FlowcellRunAnnotator(object):
     """
@@ -25,7 +26,6 @@ class FlowcellRunAnnotator(object):
             genomics_root = self._get_genomics_root()
 
         logger.debug("setting 'genomics' path")
-
         try:
             self.genomics_path = os.path.join(genomics_root, 'genomics')
         except AttributeError:
@@ -37,9 +37,12 @@ class FlowcellRunAnnotator(object):
         Try to retrieve data for the flowcell run from GenLIMS; if
         unsuccessful, create new ``FlowcellRun`` object.
         """
+        logger.info("initializing FlowcellRun instance")
         try:
+            logger.debug("getting FlowcellRun from GenLIMS")
             return genlims.get_run(_id=run_id)
         except:
+            logger.debug("creating new FlowcellRun object")
             return docs.FlowcellRun(_id=run_id)
 
     def _get_genomics_root(self):
@@ -54,6 +57,7 @@ class FlowcellRunAnnotator(object):
         Find path to flowcell folder on the server.
         """
         illumina_path = os.path.join(self.genomics_path, 'Illumina')
+        logger.debug("searching for flowcell folder")
         try:
             return [os.path.join(illumina_path, p)
                     for p in os.listdir(illumina_path)
@@ -64,4 +68,61 @@ class FlowcellRunAnnotator(object):
             ))
             raise
 
-    # def _get_unaligned_folder(self):
+    def _get_unaligned_path(self):
+        """
+        Find path to unaligned data in flowcell folder.
+        """
+        try:
+            return os.path.join(self._get_flowcell_path(), 'Unaligned')
+        except OSError:
+            logger.error("'Unaligned' folder doesn't exist")
+            raise
+
+    def get_projects(self):
+        """
+        Collect list of projects for flowcell run.
+        """
+        unaligned_path = self._get_unaligned_path()
+        logger.info("collecting list of projects")
+        return [p
+                for p in os.listdir(unaligned_path)
+                if len(illumina.get_project_label(p))]
+
+    def get_libraries(self, project=None):
+        """
+        Collect list of libraries for flowcell run from one or all projects.
+        """
+        unaligned_path = self._get_unaligned_path()
+        projects = self.get_projects()
+        if project is not None:
+            logger.debug("subsetting projects")
+            projects = [p for p in projects
+                        if re.search(project, p)]
+        logger.info("collecting list of libraries")
+        logger.debug("searching in projects {}".format(projects))
+        return [l for p in projects
+                for l in os.listdir(os.path.join(unaligned_path, p))
+                if len(illumina.get_lib_id(l))]
+
+    def get_sequenced_libraries(self, project=None):
+        """
+        Collect sequenced library objects for flowcell run.
+        """
+        unaligned_path = self._get_unaligned_path()
+        projects = self.get_projects()
+        if project is not None:
+            logger.debug("subsetting projects")
+            projects = [p for p in projects
+                        if re.search(project, p)]
+        for p in projects:
+            logger.info("getting sequenced libraries for project {}".format(p))
+            libraries = self.get_libraries(p)
+
+
+class SequencedLibraryAnnotator(object):
+    """
+    Identifies, stores, and updates information about a sequenced library.
+    """
+    def __init__(self, path):
+        logger.info("creating an instance of SequencedLibraryAnnotator")
+        self.path = path
--- tests/test_annotation.py
+++ tests/test_annotation.py
@@ -54,3 +54,43 @@ def test_flowcellrunannotator_get_flowcell_path_invalid_root():
     )
     with pytest.raises(OSError):
         assert(fcrunannotator._get_flowcell_path())
+
+def test_flowcellrunannotator_get_unaligned_path():
+    fcrunannotator = illuminaseq.FlowcellRunAnnotator(
+        '150615_D00565_0087_AC6VG0ANXX',
+        genomics_root='./tests/test-data/'
+    )
+    assert(fcrunannotator._get_unaligned_path() ==
+        ('./tests/test-data/genomics/Illumina/150615_D00565_0087_AC6VG0ANXX/'
+         'Unaligned'))
+
+def test_flowcellrunannotator_get_projects():
+    fcrunannotator = illuminaseq.FlowcellRunAnnotator(
+        '150615_D00565_0087_AC6VG0ANXX',
+        genomics_root='./tests/test-data/'
+    )
+    assert(len(fcrunannotator.get_projects()) == 8)
+    assert('P14-12-23221204' in fcrunannotator.get_projects())
+    assert('P109-1-21113094' in fcrunannotator.get_projects())
+
+def test_flowcellrunannotator_get_libraries_set_project():
+    fcrunannotator = illuminaseq.FlowcellRunAnnotator(
+        '150615_D00565_0087_AC6VG0ANXX',
+        genomics_root='./tests/test-data/'
+    )
+    assert(len(fcrunannotator.get_libraries('P14-12')) == 5)
+    assert('lib7293-25920016' in fcrunannotator.get_libraries('P14-12'))
+
+def test_flowcellrunannotator_get_libraries_all_projects():
+    fcrunannotator = illuminaseq.FlowcellRunAnnotator(
+        '150615_D00565_0087_AC6VG0ANXX',
+        genomics_root='./tests/test-data/'
+    )
+    assert(len(fcrunannotator.get_libraries()) == 36)
+
+def test_sequencedlibraryannotator_creation():
+    seqlibannotator = illuminaseq.SequencedLibraryAnnotator(
+        ('./tests/test-data/genomics/Illumina/150615_D00565_0087_AC6VG0ANXX/'
+         'P14-12-23221204/lib7293-25920016')
+    )
+    assert(seqlibannotator.path)
