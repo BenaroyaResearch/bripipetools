ede4c8fd42caab6928a080ee9408c92ddcfa1e2f
James Eddy <james.a.eddy@gmail.com>
2016-09-01 17:06:22 -0700

    add lib id tests to parsing test module

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -3,6 +3,11 @@ import pytest
 
 from bripipetools.parsing import illumina
 
+def test_get_lib_id():
+    assert(illumina.get_lib_id('lib1234-1234') == 'lib1234')
+    assert(illumina.get_lib_id('Sample_lib1234') == 'lib1234')
+    assert(illumina.get_lib_id('Sample1234') == '')
+
 def test_get_project_label_no_subproject():
     assert(illumina.get_project_label('P00Processed') == 'P00')
 
