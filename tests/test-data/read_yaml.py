a34c0cd294d531676c70adeb3c5d33fd84ac0242
James Eddy <james.a.eddy@gmail.com>
2016-09-30 09:45:39 -0700

    add initial version of new mock genomics server fixture

--- tests/test-data/read_yaml.py
+++ tests/test-data/read_yaml.py
@@ -11,15 +11,22 @@ def ujoin(loader, node):
     seq = loader.construct_sequence(node)
     return '_'.join([str(i) for i in seq])
 
-
 def join_path(d, root):
     """
     Walk down tree to recursively join paths.
     """
-    for item in d:
-        if isinstance(d[item], dict) and 'path' in d[item]:
-            d[item]['path'] = os.path.join(root, d[item]['path'])
-            d[item] = join_path(d[item], d[item]['path'])
+    if isinstance(d, dict):
+        if 'path' in d:
+            d['path'] = os.path.join(root, d['path'])
+            root = d['path']
+        for item in d:
+            d[item] = join_path(d[item], root)
+
+    elif isinstance(d, list):
+        print("!!! d:: {}, ROOT:: {}".format(d, root))
+        d = [join_path(item, root) for item in d]
+        print("!!! NEW d:: {}, ROOT:: {}".format(d, root))
+
     return d
 
 ## register the tag handler
