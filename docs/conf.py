f793109f2bcd3b63232099e9fe2c11aeae240bb2
James Eddy <james.a.eddy@gmail.com>
2016-10-07 08:18:16 -0700

    fix system path in docs config

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@
 
 import os
 import sys
-# sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration ------------------------------------------------
 
@@ -33,6 +33,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
