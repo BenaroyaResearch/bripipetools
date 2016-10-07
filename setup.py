6fffe25dabe85864f50ddd2d09fa66eb185350e0
James Eddy <james.a.eddy@gmail.com>
2016-10-07 09:20:12 -0700

    Bump version: 0.2.3 → 0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 config = {
     'name': 'bripipetools',
-    'version': '0.2.3',
+    'version': '0.3.0',
     'description': 'Software for managing BRI bioinformatics pipelines',
     'author': 'James A. Eddy',
     'author_email': 'james.a.eddy@gmail.com',
