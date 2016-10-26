c2df7b4b90dc47c5dc70b92b9da0ab9b02b6c671
James Eddy <james.a.eddy@gmail.com>
2016-10-26 15:34:45 -0700

    add some module level docstrings

--- bripipetools/parsing/__init__.py
+++ bripipetools/parsing/__init__.py
@@ -1,5 +1,14 @@
 """
-Methods for handling and parsing common string formats.
+bripipetools ``parsing`` module
+
+Slightly more specialized than methods in the ``util.strings`` module,
+provides functions for parsing and extracting information from strings
+that follow some expected nomenclature. The primary examples of this
+information are IDs, names, labels, and other metadata for files and
+objects generated either by Illumina technology or the BRI Genomics
+Core (via GenLIMS).
+
+Depends on the ``util`` module.
 """
 from .illumina import (get_project_label, parse_project_label,
                        get_library_id, get_flowcell_id, parse_flowcell_run_id,
--- bripipetools/util/__init__.py
+++ bripipetools/util/__init__.py
@@ -1,6 +1,10 @@
 """
-Utility classes and methods for basic operations with strings and files
-as well as user interaction.
+bripipetools ``util`` module
+
+Includes convenience methods related to handling and manipulating
+strings (``util.strings``), file paths (``util.files``), as well as
+user interactions via the command line (``util.ui``). Methods are used
+throughout other modules to streamline common operations.
 """
 from .strings import (matchdefault, to_camel_case, to_snake_case)
 from .files import (locate_root_folder, swap_root)
