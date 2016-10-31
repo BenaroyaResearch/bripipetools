6e8f586b3a8b5dac284bd562bf4c7b840d10fc99
James Eddy <james.a.eddy@gmail.com>
2016-10-31 11:56:25 -0700

    remove ui submodule

--- bripipetools/util/__init__.py
+++ bripipetools/util/__init__.py
@@ -6,4 +6,3 @@ throughout other modules to streamline common operations.
 """
 from .strings import (matchdefault, to_camel_case, to_snake_case)
 from .files import (locate_root_folder, swap_root)
-from .ui import (prompt_raw, list_options, input_to_int, input_to_int_list)
