f2997f7b4f85c4268616a5c7a951a0de366310be
James Eddy <james.a.eddy@gmail.com>
2017-01-10 09:39:21 -0800

    add annotation to tests for sex check qc module

--- tests/test_qc.py
+++ tests/test_qc.py
@@ -1,11 +1,9 @@
 import logging
-import os
-import re
 
-import pytest
 import mock
 import mongomock
 import pandas as pd
+import pytest
 
 from bripipetools import qc
 
@@ -25,7 +23,7 @@ def mock_db():
 
 
 @pytest.fixture(scope='function')
-def mock_countfile(s, filename, tmpdir):
+def mock_stringfile(s, filename, tmpdir):
     f = tmpdir.join(filename)
     f.write(s)
 
@@ -39,18 +37,14 @@ def mock_proclib(count_filename=None):
 
     processedlibrary = mock.Mock(
         _id='lib1111_C00000XX_processed',
-        processed_data=[
-            {'workflowbatch_id': mock_batchid,
-             'outputs': {
-                'counts': [
-                    {'source': 'htseq',
-                     'name': 'htseq_counts_txt',
-                     'file': count_filename
-                     }
-                 ]
-             }
-             }
-        ],
+        processed_data=[{
+            'workflowbatch_id': mock_batchid,
+            'outputs': {
+                'counts': [{'source': 'htseq',
+                            'name': 'htseq_counts_txt',
+                            'file': count_filename}]
+            }
+        }],
         parent_id='mock_sample',
         type='processed library')
 
@@ -64,12 +58,13 @@ class TestSexChecker:
     `bripipetools.qc.sexcheck` module.
     """
     def test_load_x_genes(self, mock_db, tmpdir):
-
-        # AND a SexChecker with mock processed library and specified
-        # workflow batch ID
+        # GIVEN data for a processed library from a specified workflow batch
+        # and and a connection to a database
         mock_object = mock_proclib()
         mock_batchid = mock_object.processed_data[0]['workflowbatch_id']
 
+        # AND a SexChecker is created for the processed library and
+        # corresponding workflow batch ID
         checker = qc.SexChecker(
             processedlibrary=mock_object,
             reference='grch38',
@@ -78,18 +73,21 @@ class TestSexChecker:
             db=mock_db
         )
 
+        # WHEN X chromosome genes are loaded from expected package file
         test_table = checker._load_x_genes()
 
+        # THEN the genes should be returned in a single column data frame
         assert (len(test_table) == 2539)
         assert (len(test_table.columns) == 1)
 
     def test_load_y_genes(self, mock_db, tmpdir):
-
-        # AND a SexChecker with mock processed library and specified
-        # workflow batch ID
+        # GIVEN data for a processed library from a specified workflow batch
+        # and and a connection to a database
         mock_object = mock_proclib()
         mock_batchid = mock_object.processed_data[0]['workflowbatch_id']
 
+        # AND a SexChecker is created for the processed library and
+        # corresponding workflow batch ID
         checker = qc.SexChecker(
             processedlibrary=mock_object,
             reference='grch38',
@@ -98,22 +96,26 @@ class TestSexChecker:
             db=mock_db
         )
 
+        # WHEN Y chromosome genes are loaded from expected package file
         test_table = checker._load_y_genes()
 
+        # THEN the genes should be returned in a single column data frame
         assert (len(test_table) == 589)
         assert (len(test_table.columns) == 1)
 
     def test_get_counts_path(self, mock_db, tmpdir):
-        # AND a SexChecker with mock processed library and specified
-        # workflow batch ID
+        # GIVEN data for a processed library from a specified workflow batch
+        # and and a connection to a database, and a file of gene counts
+        # exists for the library from the workflow batch
         mock_contents = ['ENSG00000000001\t0\n', ]
         mock_filename = 'lib1111_htseq_counts.txt'
-        mock_path = mock_countfile(''.join(mock_contents), mock_filename,
-                                   tmpdir)
-
+        mock_path = mock_stringfile(''.join(mock_contents), mock_filename,
+                                    tmpdir)
         mock_object = mock_proclib(count_filename=mock_filename)
         mock_batchid = mock_object.processed_data[0]['workflowbatch_id']
 
+        # AND a SexChecker is created for the processed library and
+        # corresponding workflow batch ID
         checker = qc.SexChecker(
             processedlibrary=mock_object,
             reference='grch38',
@@ -122,23 +124,27 @@ class TestSexChecker:
             db=mock_db
         )
 
+        # WHEN the full path to the counts file is retrieved
         test_path = checker._get_counts_path()
 
+        # THEN the path should match the expected result
         assert (test_path == mock_path)
 
     def test_get_x_y_counts(self, mock_db, tmpdir):
+        # GIVEN data for a processed library from a specified workflow batch
+        # and and a connection to a database, and a file of gene counts
+        # exists for the library from the workflow batch
         mock_contents = ['ENSG00000182888\t1\n',
                          'ENSG00000273773\t1\n',
                          'ENSG00000224873\t1\n',
                          'ENSG00000231159\t1\n', ]
-
         mock_filename = 'lib1111_htseq_counts.txt'
-        mock_path = mock_countfile(''.join(mock_contents), mock_filename,
-                                   tmpdir)
-
+        mock_stringfile(''.join(mock_contents), mock_filename, tmpdir)
         mock_object = mock_proclib(count_filename=mock_filename)
         mock_batchid = mock_object.processed_data[0]['workflowbatch_id']
 
+        # AND a SexChecker is created for the processed library and
+        # corresponding workflow batch ID
         checker = qc.SexChecker(
             processedlibrary=mock_object,
             reference='grch38',
@@ -147,8 +153,13 @@ class TestSexChecker:
             db=mock_db
         )
 
+        # WHEN X and Y chromosome specific counts are extracted from the
+        # library counts file and stored as separate attributes on the
+        # checker object
         checker._get_x_y_counts()
 
+        # THEN the 'x_counts' and 'y_counts' attributes should be data
+        # frames, containing counts for X and Y genes, respectively
         assert checker.x_counts.equals(
             pd.DataFrame([['ENSG00000182888', 1],
                           ['ENSG00000273773', 1]],
@@ -161,17 +172,20 @@ class TestSexChecker:
         )
 
     def test_compute_x_y_data(self, mock_db, tmpdir):
+        # GIVEN data for a processed library from a specified workflow batch
+        # and and a connection to a database, and a file of gene counts
+        # exists for the library from the workflow batch
         mock_contents = ['ENSG00000182888\t1\n',
                          'ENSG00000273773\t2\n',
                          'ENSG00000224873\t1\n',
                          'ENSG00000231159\t2\n', ]
-
         mock_filename = 'lib1111_htseq_counts.txt'
-        mock_countfile(''.join(mock_contents), mock_filename, tmpdir)
-
+        mock_stringfile(''.join(mock_contents), mock_filename, tmpdir)
         mock_object = mock_proclib(count_filename=mock_filename)
         mock_batchid = mock_object.processed_data[0]['workflowbatch_id']
 
+        # AND a SexChecker is created for the processed library and
+        # corresponding workflow batch ID
         checker = qc.SexChecker(
             processedlibrary=mock_object,
             reference='grch38',
@@ -180,8 +194,14 @@ class TestSexChecker:
             db=mock_db
         )
 
+        # WHEN all X and Y related count data are pre-computed, then results
+        # should be stored in a dictionary under the checker object's 'data'
+        # attribute and include fields for '_genes' (the number of genes
+        # detected with count > 0), and '_counts' (the sum of raw counts)
+        # for X and Y genes, as well as total counts
         checker._compute_x_y_data()
 
+        # THEN the summarized X/Y count data should match expected results
         assert (
             checker.data == {'x_genes': 2,
                              'y_genes': 2,
@@ -191,9 +211,13 @@ class TestSexChecker:
         )
 
     def test_predict_sex(self, mock_db, tmpdir):
+        # GIVEN data for a processed library from a specified workflow batch
+        # and and a connection to a database
         mock_object = mock_proclib()
         mock_batchid = mock_object.processed_data[0]['workflowbatch_id']
 
+        # AND a SexChecker is created for the processed library and
+        # corresponding workflow batch ID
         checker = qc.SexChecker(
             processedlibrary=mock_object,
             reference='grch38',
@@ -202,21 +226,30 @@ class TestSexChecker:
             db=mock_db
         )
 
+        # AND X/Y count summary data are stored as a dict in the object's
+        # 'data' attribute
         checker.data = {'x_genes': 2,
                         'y_genes': 2,
                         'x_counts': 3,
                         'y_counts': 3,
                         'total_counts': 6}
 
+        # WHEN the object calls a different class to predict sample sex
         checker._predict_sex()
 
-        # THEN predicted sex should match reported sex
+        # THEN predicted sex should be returned as either 'male' or 'female'
+        # (note: the behavior and performance for predicting sex is tested
+        # separately for the `sexpredict` module)
         assert (checker.data['predicted_sex'] in ['male', 'female'])
 
     def test_verify_sex(self, mock_db, tmpdir):
+        # GIVEN data for a processed library from a specified workflow batch
+        # and and a connection to a database
         mock_object = mock_proclib()
         mock_batchid = mock_object.processed_data[0]['workflowbatch_id']
 
+        # AND a SexChecker is created for the processed library and
+        # corresponding workflow batch ID
         checker = qc.SexChecker(
             processedlibrary=mock_object,
             reference='grch38',
@@ -225,34 +258,43 @@ class TestSexChecker:
             db=mock_db
         )
 
+        # AND the result of sex prediction is stored in the object's data
+        # attribute under the 'predicted_sex' field
         checker.data = {'predicted_sex': 'male'}
 
-        # WHEN sex is verified
+        # WHEN the object calls a different class to verify whether the
+        # predicted sex matches the reported sex for the corresponding
+        # subject in the database
         checker._verify_sex()
 
-        # THEN predicted sex should match reported sex
-        assert (checker.data['sex_check'] == 'NA')
+        # THEN the verification result, stored in the 'sex_check' field of
+        # the object's 'data' attribute, should indicate either 'pass',
+        # 'fail', or 'NA' (note: the behavior for verifying sex is tested
+        # separately for the `sexverify` module)
+        assert (checker.data['sex_check'] in ['pass', 'fail', 'NA'])
 
     def test_write_data(self, mock_db, tmpdir):
+        # GIVEN data for a processed library from a specified workflow batch
+        # and and a connection to a database, and a file of gene counts
+        # exists at the path formatted:
+        # <run_folder>/<project_folder>/<counts_folder>/<counts_file>
         mock_contents = ['ENSG00000182888\t1\n',
                          'ENSG00000273773\t2\n',
                          'ENSG00000224873\t1\n',
                          'ENSG00000231159\t2\n', ]
-
         mock_root = str(tmpdir)
         mock_runid = '161231_INSTID_0001_AC00000XX'
         mock_projdir = tmpdir.mkdir(mock_runid).mkdir('Project_P1-1Processed')
         mock_countdir = mock_projdir.mkdir('counts')
-
         mock_filename = 'lib1111_C00000XX_htseq_counts.txt'
-        mock_path = mock_countfile(''.join(mock_contents), mock_filename,
-                                   mock_countdir)
-
-        mock_filename = str(mock_path).replace(mock_root, '')
-
-        mock_object = mock_proclib(count_filename=mock_filename)
+        mock_path = mock_stringfile(''.join(mock_contents), mock_filename,
+                                    mock_countdir)
+        mock_countfile = str(mock_path).replace(mock_root, '')
+        mock_object = mock_proclib(count_filename=mock_countfile)
         mock_batchid = mock_object.processed_data[0]['workflowbatch_id']
 
+        # AND a SexChecker is created for the processed library and
+        # corresponding workflow batch ID
         checker = qc.SexChecker(
             processedlibrary=mock_object,
             reference='grch38',
@@ -261,6 +303,9 @@ class TestSexChecker:
             db=mock_db
         )
 
+        # AND X/Y count summary data as well as the result of sex prediction
+        # and sex verification are stored as a dict in the object's
+        # 'data' attribute
         checker.data = {'x_genes': 2,
                         'y_genes': 2,
                         'x_counts': 3,
@@ -268,33 +313,37 @@ class TestSexChecker:
                         'total_counts': 6,
                         'predicted_sex': 'male'}
 
+        # WHEN the sex check data is written to a new file under the
+        # processed project's 'validation' folder
         test_path = checker._write_data()
 
-        mock_path = (mock_projdir.join('validation')
-                     .join('lib1111_C00000XX_sexcheck_validation.csv'))
-
-        assert (test_path == str(mock_path))
+        # THEN the file should exist at the expected path
+        mock_outpath = (mock_projdir.join('validation')
+                        .join('lib1111_C00000XX_sexcheck_validation.csv'))
+        assert (test_path == str(mock_outpath))
 
     def test_update(self, mock_db, tmpdir):
+        # GIVEN data for a processed library from a specified workflow batch
+        # and and a connection to a database, and a file of gene counts
+        # exists at the path formatted:
+        # <run_folder>/<project_folder>/<counts_folder>/<counts_file>
         mock_contents = ['ENSG00000182888\t1\n',
                          'ENSG00000273773\t2\n',
                          'ENSG00000224873\t1\n',
                          'ENSG00000231159\t2\n', ]
-
         mock_root = str(tmpdir)
         mock_runid = '161231_INSTID_0001_AC00000XX'
         mock_projdir = tmpdir.mkdir(mock_runid).mkdir('Project_P1-1Processed')
         mock_countdir = mock_projdir.mkdir('counts')
-
         mock_filename = 'lib1111_C00000XX_htseq_counts.txt'
-        mock_path = mock_countfile(''.join(mock_contents), mock_filename,
-                                   mock_countdir)
-
-        mock_filename = str(mock_path).replace(mock_root, '')
-
-        mock_object = mock_proclib(count_filename=mock_filename)
+        mock_path = mock_stringfile(''.join(mock_contents), mock_filename,
+                                    mock_countdir)
+        mock_countfile = str(mock_path).replace(mock_root, '')
+        mock_object = mock_proclib(count_filename=mock_countfile)
         mock_batchid = mock_object.processed_data[0]['workflowbatch_id']
 
+        # AND a SexChecker is created for the processed library and
+        # corresponding workflow batch ID
         checker = qc.SexChecker(
             processedlibrary=mock_object,
             reference='grch38',
@@ -305,24 +354,22 @@ class TestSexChecker:
 
         # WHEN sex check validation is appended to processed data for the
         # processed library
-        processedlibrary = checker.update()
+        test_object = checker.update()
 
         # THEN the processed library object should include the expected
         # validation fields
-        assert (
-            all(
-                [field in
-                 processedlibrary.processed_data[0]['validation']['sex_check']
-                 for field in ['x_genes', 'y_genes', 'x_counts', 'y_counts',
-                               'total_counts', 'y_x_gene_ratio', 'y_x_count_ratio',
-                               'predicted_sex', 'sex_check']]
-            )
-        )
+        expected_fields = ['x_genes', 'y_genes', 'x_counts', 'y_counts',
+                           'total_counts', 'y_x_gene_ratio', 'y_x_count_ratio',
+                           'sexcheck_eqn', 'sexcheck_cutoff',
+                           'predicted_sex', 'sex_check']
+        test_fields = test_object.processed_data[0]['validation']['sex_check']
+        assert (all([field in expected_fields for field in test_fields]))
 
 
 class TestSexPredictor:
     """
-
+    Tests methods for the `SexPredictor` class in the
+    `bripipetools.qc.sexpredict` module.
     """
     def test_compute_y_x_gene_ratio(self):
         mock_data = {'x_genes': 1,
@@ -387,7 +434,8 @@ class TestSexPredictor:
 
 class TestSexVerifier:
     """
-
+    Tests methods for the `SexVerifier` class in the
+    `bripipetools.qc.sexverify` module.
     """
     def test_retrieve_sex(self, mock_db):
         # AND a SexChecker with mock processed library and specified
@@ -410,9 +458,11 @@ class TestSexVerifier:
         # AND a hierarchy of objects in the 'samples' collection, with
         # parent relationship specified by the 'parentId' field
         mock_db.samples.insert(
-            {'_id': mock_object._id, 'parentId': mock_object.parent_id})
+            {'_id': mock_object._id, 'parentId': mock_object.parent_id}
+        )
         mock_db.samples.insert(
-            {'_id': mock_object.parent_id, 'reportedSex': 'male'})
+            {'_id': mock_object.parent_id, 'reportedSex': 'male'}
+        )
 
         test_result = checker._retrieve_sex(mock_object.parent_id)
 
@@ -439,9 +489,11 @@ class TestSexVerifier:
         # AND a hierarchy of objects in the 'samples' collection, with
         # parent relationship specified by the 'parentId' field
         mock_db.samples.insert(
-            {'_id': mock_object._id, 'parentId': mock_object.parent_id})
+            {'_id': mock_object._id, 'parentId': mock_object.parent_id}
+        )
         mock_db.samples.insert(
-            {'_id': mock_object.parent_id, 'reportedSex': 'male'})
+            {'_id': mock_object.parent_id, 'reportedSex': 'male'}
+        )
 
         test_data = checker.verify()
 
