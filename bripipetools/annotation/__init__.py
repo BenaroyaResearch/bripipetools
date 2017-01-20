27aa71b348f55e67f2f83431e61b4d03ba93276f
James Eddy <james.a.eddy@gmail.com>
2017-01-20 08:44:46 -0800

    cleanup and expand package level docstrings

--- bripipetools/annotation/__init__.py
+++ bripipetools/annotation/__init__.py
@@ -2,7 +2,7 @@
 Includes critical functionality for identifying, locating, and
 describing data and results at various points (e.g., data generation,
 computational processing) in the bioinformatics pipeline. Each
-"annotator" class, contained in its respective submodule, is
+"annotator" class, contained in its respective module, is
 responsible for collecting and/or updating information for a specific
 object in the GenLIMS database. When possible, details for an object
 are retrieved directly from the database; for new objects or objects
--- bripipetools/util/__init__.py
+++ bripipetools/util/__init__.py
@@ -2,7 +2,7 @@
 Includes convenience methods related to handling and manipulating
 strings (``util.strings``), file paths (``util.files``), as well as
 user interactions via the command line (``util.ui``). Methods are used
-throughout other modules to streamline common operations.
+throughout other packages to streamline common operations.
 """
 from .strings import (matchdefault, to_camel_case, to_snake_case)
 from .files import (locate_root_folder, swap_root)
