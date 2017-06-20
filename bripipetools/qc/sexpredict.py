ce027dba0c20d4956210c958aa6cd59f4bf6d468
Mario Rosasco <mrosasco@benaroyaresearch.org>
2017-06-19 17:41:04 -0700

    Added divide-by-zero checking in the SexPredictor class functions, and started working on making sex prediction flexible (regarding cutoff and sex prediction equation)

--- bripipetools/qc/sexpredict.py
+++ bripipetools/qc/sexpredict.py
@@ -19,17 +19,31 @@ class SexPredictor(object):
         Calculate the ratio of Y genes detected to X genes detected,
         where detected = count > 0.
         """
-        self.data['y_x_gene_ratio'] = (float(self.data['y_genes'])
+        n_y = float(self.data['y_genes'])
+        n_x = float(self.data['x_genes'])
+        if n_x == 0:
+            # for now, set ot number of y counts.
+            # might be better to set to something very large,
+            # or to cutoff + n_y? This nicely handles instance where
+            # n_y = n_x = 0
+            self.data['y_x_gene_ratio'] = n_y
+        else:
+            self.data['y_x_gene_ratio'] = (float(self.data['y_genes'])
                                        / float(self.data['x_genes']))
 
     def _compute_y_x_count_ratio(self):
         """
         Calculate the ratio of Y counts to X counts.
         """
-        self.data['y_x_count_ratio'] = (float(self.data['y_counts'])
-                                        / float(self.data['x_counts']))
+        n_y = float(self.data['y_counts'])
+        n_x = float(self.data['x_counts'])
+        if n_x == 0:
+            self.data['y_x_count_ratio'] = n_y
+        else:
+            self.data['y_x_count_ratio'] = (float(self.data['y_counts'])
+                                       / float(self.data['x_counts']))
 
-    def _predict_sex(self):
+    def _predict_sex(self, cutoff=1, equation='y_sq_over_tot'):
         """
         Return predicted sex based on X/Y gene equation and cutoff.
         """
@@ -41,8 +55,12 @@ class SexPredictor(object):
         logger.debug("ratio of Y counts to X counts: {}"
                      .format(self.data['y_x_count_ratio']))
 
-        cutoff = 1
-        equation = '(y_counts^2 / total_counts) > cutoff'
+        possible_eqs={
+            'y_sq_over_tot': '(y_counts^2 / total_counts) > cutoff',
+            'gene_ratio': '(y_genes / x_genes) > cutoff',
+            'count_ratio': '(y_counts / x_counts) > cutoff'
+        }
+        equation = possible_eqs[equation]
         logger.debug("using equation: {}".format(equation))
         value = (float(self.data['y_counts']**2)
                  / float(self.data['total_counts']))
