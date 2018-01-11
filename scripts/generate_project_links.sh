4bfc7f99b1bb9e84636154eb14b851a4d09ae4e5
Mario Rosasco <mrosasco@benaroyaresearch.org>
2018-01-11 09:14:25 -0800

    Project links now sorted, and contain full SEA-ZED address name

--- scripts/generate_project_links.sh
+++ scripts/generate_project_links.sh
@@ -5,10 +5,10 @@ FC_DIR=${1%/}
 while read proj_dir; do
     proj=$(echo $proj_dir | egrep "P[0-9]+(-[0-9]+)*" -o)
     echo $proj
-    smb_path=$(echo $proj_dir | awk '{gsub(/\/(mnt|Volumes)/, "smb://sea-zed-01")}1')
+    smb_path=$(echo $proj_dir | awk '{gsub(/\/(mnt|Volumes)/, "smb://sea-zed-01.brivmrc.org")}1')
     echo "Mac: ${smb_path}"
-    win_path=$(echo $smb_path | awk '{gsub("smb:", "")}1' | awk '{gsub("/", "\\")}1')
+    win_path=$(echo $smb_path | awk '{gsub("smb:", "")}1' | awk '{gsub("/", "\\")}1' | sed 's/.brivmrc.org//')
     echo "Windows: ${win_path}"
     echo " "
 
-done < <(find $FC_DIR -maxdepth 1 -name "Project_P*Processed*")
+done < <(find $FC_DIR -maxdepth 1 -name "Project_P*Processed*" | sort)
