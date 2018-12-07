f5f638d758c9cec8d24889a204851cf1a1936fc9
Mario Rosasco <mrosasco@benaroyaresearch.org>
2018-12-07 13:37:40 -0800

    added call to researchDB API to get/parse project descriptions for proj links

--- scripts/generate_project_links.sh
+++ scripts/generate_project_links.sh
@@ -4,7 +4,9 @@ FC_DIR=${1%/}
 
 while read proj_dir; do
     proj=$(echo $proj_dir | egrep "P[0-9]+(-[0-9]+)*" -o)
-    echo $proj
+    projDetails=$(curl -s https://gcq.benaroyaresearch.org/project/projectByNumber/$proj | 
+      grep -Eo \"shortTitle\"\:.+\"projName\" | sed 's/\"shortTitle\"\://g;s/,\"projName\"//g')
+    echo $proj $projDetails
     smb_path=$(echo $proj_dir | awk '{gsub(/\/(mnt|Volumes)/, "smb://isilon.brivmrc.org")}1')
     echo "Mac: ${smb_path}"
     win_path=$(echo $smb_path | awk '{gsub("smb:", "")}1' | awk '{gsub("/", "\\")}1' | sed 's/.brivmrc.org//')
